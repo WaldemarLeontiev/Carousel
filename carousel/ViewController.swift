091cfe1f46cde5804deb0c3aa954fcac2cdb9196
Waldemar Leontiev <waldemar.leontiev@gmail.com>
2018-06-25 16:08:10 +0300

    Initial Commit

---
+++ carousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  carousel
+//
+//  Created by Waldemar on 25/06/2018.
+//  Copyright © 2018 h7.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
